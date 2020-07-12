16b988e4250dd9983bc87add831936acefc2bc61
Erin Yilmaz <64546500+erinyil@users.noreply.github.com>
2020-07-12 12:29:34 -0700

    Create segment.h

---
+++ segment.h
@@ -0,0 +1,16 @@
+/*
+ * Erin Yilmaz and Gustavo Curioso
+ * eyilma02 and gcurio01
+ * HW 6 (UM)
+ * segment.h
+ */
+
+#ifndef _SEGMENT_H
+#define _SEGMENT_H
+
+typedef struct Segment {
+    uint32_t *seg;
+    uint32_t length;
+} *Segment;
+
+#endif
